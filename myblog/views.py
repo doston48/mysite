cfe5c33ba24f03911e14648454392a547b03cad3
doston <dost7ony6@gmail.com>
2025-12-28 12:27:38 -0500

    changes you made

--- myblog/views.py
+++ myblog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def homepage (request) :
+def homepage(request):
   return HttpResponse("<h1>This is homepage! Welcome to Django!</h1>")
 
-def about (request):
+def about(request):
   return HttpResponse("<hl>This is about page!</h1>")
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('', include("myapp.urls")),
-  path( 'blog/', include("myblog.urls")),
+  
+  path('blog/',include("myblog.urls")),
  ]
